95834d80077bc0ede895caff1b9aaba15fc02a09
Ping <panfilo.glophics@gmail.com>
2021-03-02 21:22:47 +0800

    client store

--- resources/views/file_replacement/index.blade.php
+++ resources/views/file_replacement/index.blade.php
@@ -119,7 +119,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">Product Report Images</h4>
+                <h4 class="modal-title">Attached Files</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
