eae60ccdcd709e56f8ca6ad3a022edadc0885aaa
Ping <panfilo.glophics@gmail.com>
2021-03-02 20:06:21 +0800

    refactor order

--- app/Http/Controllers/TransactionHistoryController.php
+++ app/Http/Controllers/TransactionHistoryController.php
@@ -29,17 +29,49 @@ class TransactionHistoryController extends Controller
      */
     public function index(Request $request)
     {
-        $order = DB::table('orders')
-                ->join('products', 'orders.product_id', '=', 'products.id')
-                ->select('products.name', 'products.product_image', 'orders.quantity_ordered',
-                    'orders.ordered_total_price', 'orders.created_at', 'orders.is_approved', 'orders.is_completed', 'orders.delivery_date', 'orders.id', 'orders.cancelled_by')
-                ->where('client_id', Auth::user()->id)
+        // $order = DB::table('orders')
+        //         ->join('products', 'orders.product_id', '=', 'products.id')
+        //         ->select('products.name', 'products.product_image', 'orders.quantity_ordered',
+        //             'orders.ordered_total_price', 'orders.created_at', 'orders.is_approved', 'orders.is_completed', 'orders.delivery_date', 'orders.id', 'orders.cancelled_by')
+        //         ->where('client_id', Auth::user()->id)
+        //         ->get();
+        $order = DB::table('order_invoice')
+                ->join('orders', 'orders.invoice_id', '=', 'order_invoice.id')
+                ->join('users', 'orders.client_id', '=', 'users.id')
+                ->selectRaw("order_invoice.id, 
+                order_invoice.created_at as date_ordered, 
+                order_invoice.invoice_no, 
+                SUM(orders.ordered_total_price) as total_price, 
+                CONCAT(users.fname, ' ', users.lname) as fullname, 
+                users.email, 
+                orders.delivery_date,
+                orders.is_approved,
+                orders.is_completed,
+                orders.is_cancelled,
+                orders.delivery_date,
+                users.id as client_id,
+                users.lname,
+                users.contact_num,
+                orders.attempt,
+                orders.reason")
+                // ->select('products.id AS prodID', 'products.name', 'products.product_image', 'orders.quantity_ordered',
+                //     'orders.ordered_total_price', 'orders.created_at', 'orders.is_approved', 'orders.is_completed', 'orders.delivery_date', 'orders.id', 'users.fname', 'users.lname', 'users.contact_num', 'orders.client_id')
+                // ->where('is_approved', 0)
+                ->where('users.id', Auth::user()->id)
+                ->groupBy('orders.invoice_id')
                 ->get();
 
         if ($request->ajax()) {
             return Datatables::of($order)
                 ->addIndexColumn()
-                ->rawColumns(['action'])
+                ->addColumn('total_price', function($row){
+                    return '<strong>'.number_format($row->total_price,2).'</strong>';
+                  })
+                ->addColumn('action', function ($row) {
+                    $btn = '<a data-invoice="'.$row->invoice_no.'" data-num="'.$row->contact_num.'" href="javascript:void(0)" data-toggle="tooltip" data-placement="top" title="Update Order" data-contact data-client="'.$row->client_id.'" data-id="'.$row->id.'" data-original-title="Edit" class="btn btn-primary btn-sm viewCompletedOrder">View details</a>';
+                    return $btn;
+                })
+                ->rawColumns(['action','total_price'])
                 ->make(true);
         }
 
--- resources/views/client/transaction_history.blade.php
+++ resources/views/client/transaction_history.blade.php
@@ -7,7 +7,7 @@
             <h4 class="center">Orders History</h4>
         </div>
     </div>
-    <table id="dataTable" class="table table-striped table-bordered">
+    {{-- <table id="dataTable" class="table table-striped table-bordered">
         <thead class="bg-indigo-1 text-white">
         <tr>
             <th>ID</th>
@@ -22,9 +22,63 @@
         </thead>
         <tbody>
         </tbody>
+    </table> --}}
+    <table style="width: 100%;" id="dataTable" class="table table-striped table-bordered">
+        <thead class="bg-indigo-1 text-white">
+            <tr>
+                <th>ID</th>
+                <th>Invoice #</th>
+                {{-- <th>Customer</th> --}}
+                <th>Total</th>
+                <th>Date Ordered</th>
+                <th>Delivery Date</th>
+                {{-- <th>Attempt</th> --}}
+                {{-- <th>Reason</th> --}}
+                <th>Status</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
     </table>
     <br>
-
+    <div class="modal fade" id="HistoryModal" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Order History</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    {{-- <form id="frmPendingOrder" name="frmPendingOrder" class="form-horizontal"> --}}
+                        <table class="table table-stripped" id="completed_orders">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Product</th>
+                                    <th>Size</th>
+                                    <th>Image</th>
+                                    <th>Qty</th>
+                                    <th>Sub Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                            <tfoot>
+                                <tr>  
+                                    <td colspan="4">&nbsp;</td>  
+                                    <td>Total:</td>
+                                    <td><strong id="history_total">0</strong></td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    {{-- </form> --}}
+                </div>
+            </div>
+        </div>
+    </div>
     <script type="text/javascript">
         $(() => {
 
@@ -35,6 +89,35 @@
                 }
             });
 
+            function getCompletedOrders(invoice_id){
+            $.getJSON( "{{ url('order/completed') }}" + '/'+ invoice_id, function( data ) {
+                var htmlData = ''
+                var total = 0;
+                var i = 0
+                $.each(data, function( index, row ) {
+                    total += row.ordered_total_price
+                    htmlData += `<tr>
+                        <td>${row.id}</td>
+                        <td>${row.name}</td>
+                        <td>${row.size}</td>
+                        <td><a data-fancybox='' href='/img/product/${row.product_image}'><img src='/img/product/${row.product_image}' height='20'></a></td>
+                        <td>${row.quantity_ordered}</td>
+                        <td>${row.ordered_total_price}</td>
+                    </tr>`
+                    i++
+                });
+                $("#completed_orders").find('tbody').html("").append(htmlData) 
+                $("#history_total").text(total.toFixed(2)) 
+                $('#HistoryModal').modal('show');
+            });
+        }
+
+            $('body').on('click', '.viewCompletedOrder', function () {
+                //get the data
+                const invoice_id = $(this).attr("data-id");
+                getCompletedOrders(invoice_id)
+            });
+
             // datatable
             var table = $('#dataTable').DataTable({
                 processing: true,
@@ -42,68 +125,137 @@
                 ajax: "{{ url('transaction_history') }}",
                 columns: [
                     // {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+                    // {data: 'id', name: 'id'},
+                    // {data: 'name', name: 'name'},
+                    // {   
+                    //     data: 'product_image', name: 'product_image',
+                    //     "render": function (data, type, full, meta) {
+                    //         return "<a data-fancybox='' href='{{ URL('img/product') }}/"+ data +"'><img src='{{ URL('img/product') }}/"+ data +"' height='20'></a>";
+                    //     },
+                    // },
+                    // {
+                    //     data: 'quantity_ordered', name: 'quantity_ordered',
+                    //     "render": function(data, type, full, meta){
+                    //         return data + " pcs"
+                    //     }
+                    // },
+                    // {
+                    //     data: 'ordered_total_price', name: 'ordered_total_price',
+                    //     "render": function(data, type, full, meta){
+                    //         return "&#x20b1; " + data
+                    //     }
+                    // },
+                    // {
+                    //     data: 'created_at', name: 'created_at',
+                    //     "render": function (data, type, full, meta) {
+                    //         return moment(data).format('MMMM D YYYY, h:mm:ss a');
+                    //     },
+                    // },
+                    // {
+                    //     data: 'delivery_date', name: 'delivery_date',
+                    //     "render": function (data, type, full, meta) {
+                    //         let output = '';
+                    //         if(data === null){
+                    //             output = '<span class="text-info font-weight-bold">(Not set)</span>'
+                    //         }else{
+                    //             output = moment(data).format('MMMM D YYYY, h:mm:ss a');
+                    //         }
+
+                    //         return output
+                    //     },
+                    // },
+                    // {
+                    //     data: 'is_approved', name: 'is_approved',
+                    //     "render": function (data, type, full, meta) {
+
+                    //         let output = '';
+
+                    //         if(data === 1){
+                    //             output = '<span class="text-info font-weight-bold">Approved</span>'
+
+                    //             if(full.is_completed === 1){
+                    //                 output = '<span class="text-success font-weight-bold">Completed</span>'
+                    //             }
+                    //         }else{
+                    //             output = '<span class="text-danger font-weight-bold">Pending</span>'
+                    //         }
+
+                    //         if(full.cancelled_by > 0){
+                    //             output = '<span class="text-danger font-weight-bold">Cancelled</span>'
+                    //         }
+
+                    //         return output
+                    //     },
+                    // },
                     {data: 'id', name: 'id'},
-                    {data: 'name', name: 'name'},
-                    {   
-                        data: 'product_image', name: 'product_image',
-                        "render": function (data, type, full, meta) {
-                            return "<a data-fancybox='' href='{{ URL('img/product') }}/"+ data +"'><img src='{{ URL('img/product') }}/"+ data +"' height='20'></a>";
-                        },
+                {data: 'invoice_no', name: 'invoice_no'},
+                // {
+                //     data: 'fullname', name: 'fullname',
+                //     "render": function(data, type, full, meta){
+                //         return full.fullname
+                //     }
+                // },
+                {data: 'total_price', name: 'total_price'},
+                {
+                    data: 'date_ordered', name: 'date_ordered',
+                    "render": function (data, type, full, meta) {
+                        return moment(data).format('MMMM D YYYY');
                     },
-                    {
-                        data: 'quantity_ordered', name: 'quantity_ordered',
-                        "render": function(data, type, full, meta){
-                            return data + " pcs"
+                },
+                {
+                    data: 'delivery_date', name: 'delivery_date',
+                    "render": function (data, type, full, meta) {
+                        let output = '';
+                        if(full.delivery_date == null){
+                            output = '<span class="text-info font-weight-bold">(Not set)</span>'
+                        }else{
+                            output = moment(data).format('MMMM D YYYY');
                         }
+
+                        return output
                     },
-                    {
-                        data: 'ordered_total_price', name: 'ordered_total_price',
-                        "render": function(data, type, full, meta){
-                            return "&#x20b1; " + data
+                },
+                // {
+                //     data: 'attempt', name: 'attempt',
+                //     render: function(data, type, full, meta){
+                        
+                //         let output = parseInt(data) + 1
+                //         let times = output > 1 ? "times" : "time"
+
+                //         return output + " " + times
+                //     }
+                // },
+                
+                
+                // {
+                //     data: 'reason', name: 'reason',
+                //     render: function(data, type, full, meta){
+                //         let output = ''
+                //         if(data != ""){
+                //             output = "<a href='#' class='btnDisplayReason' data-reason='"+data+"'>View</a>"
+                //         }
+                //         return output
+                //     }
+                // },
+                {
+                    data: 'is_completed', name: 'is_completed',
+                    render: function(data, type, full, meta){
+                        let output = full.is_approved == 1 ? '<span class="text-info font-weight-bold">Approved</span><br/>' : '<span class="text-danger font-weight-bold">Pending</span><br/>';
+
+                        if(full.is_completed == 1){
+                            output += '<span class="text-success font-weight-bold">Completed</span>'
                         }
-                    },
-                    {
-                        data: 'created_at', name: 'created_at',
-                        "render": function (data, type, full, meta) {
-                            return moment(data).format('MMMM D YYYY, h:mm:ss a');
-                        },
-                    },
-                    {
-                        data: 'delivery_date', name: 'delivery_date',
-                        "render": function (data, type, full, meta) {
-                            let output = '';
-                            if(data === null){
-                                output = '<span class="text-info font-weight-bold">(Not set)</span>'
-                            }else{
-                                output = moment(data).format('MMMM D YYYY, h:mm:ss a');
-                            }
+                        if(full.is_cancelled == 1){
+                            output += '<span class="text-danger font-weight-bold">Cancelled</span>'
+                        }
+                        if(full.is_rescheduled == 1){
+                            output += '<span class="text-info font-weight-bold">Rescheduled</span>'
+                        }
 
-                            return output
-                        },
-                    },
-                    {
-                        data: 'is_approved', name: 'is_approved',
-                        "render": function (data, type, full, meta) {
-
-                            let output = '';
-
-                            if(data === 1){
-                                output = '<span class="text-info font-weight-bold">Approved</span>'
-
-                                if(full.is_completed === 1){
-                                    output = '<span class="text-success font-weight-bold">Completed</span>'
-                                }
-                            }else{
-                                output = '<span class="text-danger font-weight-bold">Pending</span>'
-                            }
-
-                            if(full.cancelled_by > 0){
-                                output = '<span class="text-danger font-weight-bold">Cancelled</span>'
-                            }
-
-                            return output
-                        },
-                    },
+                        return output
+                    }
+                },
+                {data: 'action', name: 'action', orderable: false, searchable: false},
                 ]
             });
         })
