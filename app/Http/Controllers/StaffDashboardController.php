d3c5c982fbe5366aeefe399eab2bd7350d64df0f
Ping <panfilo.glophics@gmail.com>
2021-03-14 14:28:31 +0800

    custom css

--- app/Http/Controllers/StaffDashboardController.php
+++ app/Http/Controllers/StaffDashboardController.php
@@ -292,11 +292,11 @@ class StaffDashboardController extends Controller
                 ->where('stores.area_id', $area->id)
                 ->when($request->filter_status, function($sql) use($request){
                     if($request->filter_status == 'completed'){
-                        return $sql->where(['orders.is_approved' => 1, 'orders.is_completed' => 1]);
+                        return $sql->where('orders.is_completed', 1);
                     } else if($request->filter_status == 'cancelled'){
                         return $sql->where('orders.is_cancelled', 1);
                     } else {
-                        return $sql->where(['orders.is_approved' => 1, 'orders.is_cancelled' => 0]);
+                        return $sql->where(['orders.is_approved' => 1, 'orders.is_cancelled' => 0, 'orders.is_completed' => 0]);
                     }
                 })
                 ->groupBy('orders.invoice_id')
